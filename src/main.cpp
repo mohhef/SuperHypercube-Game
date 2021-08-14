83742925297e3bf958715d50b74f90f5be7147a1
mohhef <mohhef@rocketmail.com>
2021-08-14 11:52:49 -0400

    add text

--- src/main.cpp
+++ src/main.cpp
@@ -253,6 +253,7 @@ int main(int argc, char* argv[])
 			else
 				textRendering.RenderText(*textShader, "Time: " + to_string(minutes) + ":" + to_string(seconds), 800.0f, 700.0f, 0.75f, glm::vec3(0.5, 0.8f, 0.2f));
 			textRendering.RenderText(*textShader, "Number of cubes in cluster : " + to_string(numCubes), 50.0f, 650.0f, 0.75f, glm::vec3(0.5, 0.8f, 0.2f));
+			textRendering.RenderText(*textShader, "Multiplier: " + to_string(scoreMultiplier), 50.0f, 600.0f, 0.75f, glm::vec3(0.5, 0.8f, 0.2f));
 			textRendering.disable();
 
 			// End frame
